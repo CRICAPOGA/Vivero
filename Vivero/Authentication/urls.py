3ae68e81f2dd4a99e48763d1964417cad7ac6b54
CRICAPOGA <camilochoposada2@gmail.com>
2025-03-04 07:51:56 -0500

    Login funcional sin redirección

---
+++ Vivero/Authentication/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('login_auth/', views.login_auth, name='login_auth'),
+    # path('logout/', views.logout_view, name='logout'),
+    # path('register/', views.register_view, name='register'),
+    # path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
+    # path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
+    # path('usuarios/editar/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),
+    # path('usuarios/eliminar/<int:usuario_id>/', views.eliminar_usuario, name='eliminar_usuario'),
+]
--- Vivero/Authentication/views.py
+++ Vivero/Authentication/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
+def login_view(request):
+    return render(request, 'login.html')
+
+def login_auth(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        contraseña = request.POST['password']
+        user = authenticate(request, username=username, password=contraseña)
+        if user is not None:
+            login(request, user)  # Inicia la sesión del usuario
+            messages.success(request,'Logeado')
+            return render(request, 'login.html')
+        else:
+            messages.error(request,'Credenciales incorrectas')
+            return render(request, 'login.html')
+    return render(request, 'login.html')
