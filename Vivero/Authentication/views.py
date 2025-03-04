637aeae908179ed4d40823afad868d7cbaf72b16
CRICAPOGA <camilochoposada2@gmail.com>
2025-03-04 08:50:38 -0500

    Register y roles del sistema

--- Vivero/Authentication/views.py
+++ Vivero/Authentication/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import User, Role
 
 # Create your views here.
 def login_view(request):
@@ -26,3 +27,29 @@ def login_auth(request):
 def logout_view(request):
     logout(request)
     return render(request, 'login.html')
+
+def register_view(request):
+    roles = Role.objects.all()
+    
+    if request.method == 'POST':
+        name = request.POST['name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        role_id = request.POST.get('role_id')
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        user.name = name
+        user.last_name = last_name
+        user.role_id = Role.objects.get(id=role_id) if role_id else None
+        user.save()
+        
+        messages.success(request, 'Usuario registrado exitosamente')
+        return redirect('login')
+    
+    return render(request, 'register.html', {'roles': roles})
