bae015c3923abad083c863116d19453597a1661c
CRICAPOGA <camilochoposada2@gmail.com>
2025-03-09 13:21:00 -0500

    CRUD usuarios

--- Vivero/Authentication/urls.py
+++ Vivero/Authentication/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('login_auth/', views.login_auth, name='login_auth'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
-    # path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
-    # path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
-    # path('usuarios/editar/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),
-    # path('usuarios/eliminar/<int:usuario_id>/', views.eliminar_usuario, name='eliminar_usuario'),
+    path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
+    path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
+    path('usuarios/editar/<int:usuario_id>/', views.editar_usuario, name='editar_usuario'),
+    path('usuarios/eliminar/<int:usuario_id>/', views.eliminar_usuario, name='eliminar_usuario'),
 ]
--- Vivero/Authentication/views.py
+++ Vivero/Authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -57,3 +57,63 @@ def register_view(request):
         return redirect('login')
     
     return render(request, 'register.html', {'roles': roles})
+
+############## CRUD USUARIOS #################
+@login_required
+def lista_usuarios(request):
+    usuarios = User.objects.all()
+    return render(request, 'CRUD usuarios/usuarios.html', {'usuarios': usuarios})
+
+@login_required
+def crear_usuario(request):
+    roles = Role.objects.all()
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        name = request.POST["name"]
+        last_name = request.POST["last_name"]
+        password = request.POST["password"]
+        role_id = request.POST["role_id"]
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "El nombre de usuario ya existe.")
+        else:
+            role = get_object_or_404(Role, pk=role_id)
+            usuario = User.objects.create_user(
+                username=username, email=email, name=name,
+                last_name=last_name, password=password, role_id=role
+            )
+            usuario.save()
+            messages.success(request, "Usuario creado exitosamente.")
+            return redirect("lista_usuarios")
+    
+    return render(request, "CRUD usuarios/crear_usuario.html", {"roles": roles})
+
+@login_required
+def editar_usuario(request, usuario_id):
+    usuario = get_object_or_404(User, pk=usuario_id)
+    roles = Role.objects.all()
+    
+    if request.method == "POST":
+        usuario.username = request.POST["username"]
+        usuario.email = request.POST["email"]
+        usuario.name = request.POST["name"]
+        usuario.last_name = request.POST["last_name"]
+        role_id = request.POST["role_id"]
+        usuario.role_id = get_object_or_404(Role, pk=role_id)
+        
+        if "password" in request.POST and request.POST["password"]:
+            usuario.set_password(request.POST["password"])
+        
+        usuario.save()
+        messages.success(request, "Usuario actualizado correctamente.")
+        return redirect("lista_usuarios")
+    
+    return render(request, "CRUD usuarios/editar_usuario.html", {"usuario": usuario})
+
+@login_required
+def eliminar_usuario(request, usuario_id):
+    usuario = get_object_or_404(User, pk=usuario_id)
+    usuario.delete()
+    messages.success(request, "Usuario eliminado correctamente.")
+    return redirect("lista_usuarios")
