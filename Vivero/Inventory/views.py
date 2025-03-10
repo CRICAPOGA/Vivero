1fac487e54a363b3e32f210f1de40ce37ed2c006
CRICAPOGA <camilochoposada2@gmail.com>
2025-03-10 18:22:45 -0500

    Manejo de errores en renderizado y eliminación de excel

--- Vivero/Inventory/views.py
+++ Vivero/Inventory/views.py
@@ -13,32 +13,41 @@ class Vista_subir_excel(View):
 
         # Guardar temporalmente el archivo
         file_path = default_storage.save(file.name, file)
-        
-        # Leer el archivo Excel
-        df = pd.read_excel(file_path)
+        try:
+            # Leer el archivo Excel
+            df = pd.read_excel(file_path)
 
-        for _, row in df.iterrows():
-            category_name = row['category']
-            plant_name = row['plant_name']
-            care = row['care']
-            price = row['price']
-            stock = row['stock']
+            for _, row in df.iterrows():
+                category_name = row['category']
+                plant_name = row['plant_name']
+                care = row['care']
+                price = row['price']
+                stock = row['stock']
 
-            # Buscar o crear la categoría
-            category, _ = Categorie.objects.get_or_create(category=category_name)
+                # Buscar o crear la categoría
+                category, _ = Categorie.objects.get_or_create(category=category_name)
 
-            # Buscar la planta, si existe actualiza el stock, si no, créala
-            plant, created = Plant.objects.get_or_create(
-                plant_name=plant_name,
-                category_id=category,
-                defaults={'care': care, 'price': price, 'stock': stock}
-            )
+                # Buscar la planta, si existe actualiza el stock, si no, créala
+                plant, created = Plant.objects.get_or_create(
+                    plant_name=plant_name,
+                    category_id=category,
+                    defaults={'care': care, 'price': price, 'stock': stock}
+                )
 
-            if not created:
-                plant.stock += stock  # Sumar stock existente
-                plant.save()
+                if not created:
+                    plant.stock += stock  # Sumar stock existente
+                    plant.save()
 
-        return JsonResponse({'message': 'Stock actualizado correctamente'})
+            return JsonResponse({'message': 'Stock actualizado correctamente'})
+        except Exception as e:
+            message = {'error': f'Ocurrió un error: {str(e)}'}
+
+        finally:
+            # Eliminar el archivo después de procesarlo
+            if default_storage.exists(file_path):
+                default_storage.delete(file_path)
+
+        return JsonResponse(message)
 
 def ejemplo(request):
-    render('ejemplo.html')
+    return render(request, 'ejemplo.html')
